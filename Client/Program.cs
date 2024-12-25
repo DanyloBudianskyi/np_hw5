ce9d3c7fd5d79cb69caca78105e7d2c9cd0f2fef
Danil <super.danil501@gmail.com>
2024-12-24 23:08:25 -0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Client
+    {
+        private TcpClient _Client { get; set; }
+        private NetworkStream _Stream { get; set; }
+        public async Task Start()
+        {
+            _Client = new TcpClient("127.0.0.1", 5000);
+            _Stream = _Client.GetStream();
+            Console.WriteLine("Enter your name:");
+            string clientName = Console.ReadLine();
+            await SendMessage(clientName);
+            _ = ReceiveMessage();
+            while (true)
+            {
+                Console.WriteLine("Please input message(format Name:message):");
+                string message = Console.ReadLine();
+                SendMessage(message);
+            }
+
+        }
+        private async Task ReceiveMessage()
+        {
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int bytesRead = await _Stream.ReadAsync(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    Console.WriteLine("Disconnected from server.");
+                    break;
+                }
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Message received: {message}");
+            }
+        }
+
+        private async Task SendMessage(string message)
+        {
+            if (_Stream != null)
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                await _Stream.WriteAsync(buffer, 0, buffer.Length);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.Start();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        private TcpListener Listener { get; set; }
+        private Dictionary<string,TcpClient> Clients = new Dictionary<string,TcpClient>();
+        public void Start()
+        {
+            Listener = new TcpListener(IPAddress.Any, 5000);
+            Listener.Start();
+            while (true)
+            {
+                TcpClient client = Listener.AcceptTcpClient();
+                Console.WriteLine("The client is connected...");
+                _ = HandleClient(client);
+            }
+        }
+
+        protected async Task HandleClient(object? obj)
+        {
+            TcpClient client = obj as TcpClient;
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            string clientName = null;
+            try
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                clientName = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
+                if (!Clients.TryAdd(clientName, client))
+                {
+                    Console.WriteLine("Client already exists");
+                    client.Close();
+                    return;
+                }
+                Console.WriteLine("Client connected " + clientName);
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    var parts = message.Split(":", 2);
+                    if (parts.Length == 2)
+                    {
+                        string targetId = parts[0].Trim();
+                        string msg = parts[1].Trim();
+                        SendMessage(targetId, $"{clientName}: {msg}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Client disconnected");
+                Clients.Remove(clientName);
+                client.Close();
+            }
+        }
+
+        private async Task SendMessage(string targetId, string message)
+        {
+            if (Clients.TryGetValue(targetId, out var targetClient)) {
+                NetworkStream stream = targetClient.GetStream();
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length); 
+            }
+        }
+        public void Stop()
+        {
+            Listener.Stop();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Start();
+        }
+    }
+}
